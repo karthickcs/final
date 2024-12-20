49a78f214d40de9219f6e6588c8f96f9a13993c3
karthickcs <cs.karthick@gmail.com>
2024-12-19 20:28:08 -0500

    add

--- checkmate/startpython.bat
+++ checkmate/startpython.bat
@@ -1,12 +1,12 @@
 echo %cd%
 @echo off 
-set /a inst=5 
+set /a inst=1 
 set basename=%cd% 
 cd %basename%\CheckMate\
 @echo off
 set /a loop=0
 :repeat
-start "" cmd /c CheckMate.exe %loop% %inst% 
+start "" cmd /c CheckMate.exe %loop% %inst% 50
 set /a loop=%loop%+1
 if %loop%==%inst% (
 goto escapedfromrepeat
